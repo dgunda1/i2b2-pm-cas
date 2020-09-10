a720e80be3c3523e86faf22d4526d798ab762ef1
Dileep Gunda <dgunda.gsu@gmail.com>
2020-09-10 11:24:30 -0400

    added more log

--- src/edu/harvard/i2b2/pm/delegate/ServicesHandlerCAS.java
+++ src/edu/harvard/i2b2/pm/delegate/ServicesHandlerCAS.java
@@ -81,6 +81,8 @@ public class ServicesHandlerCAS extends ServicesHandler {
 	
 	log.debug("Inside VAlidate Pass service CAS");
 	MessageContext context = MessageContext.getCurrentMessageContext();
+	log.debug("+++Context+++"+context);
+	log.debug("+++Chceking the MessageContext object+++"+ MessageContext);
 	HttpServletRequest  request = (HttpServletRequest) context.getProperty("transport.http.servletRequest");
 	System.out.println("+++request+++"+request);
 	log.debug("+++request+++"+request);
